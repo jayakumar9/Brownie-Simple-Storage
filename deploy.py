8a387c2dc27dbf651937b0678e77bb3c49f0c0ac
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-10-31 17:42:15 +0530

    Create deploy.py
    update now

---
+++ deploy.py
@@ -0,0 +1,11 @@
+#create new file under Brownie-Simple-Storage/scripts
+from brownie import accounts
+
+def deploy_simple_storage():
+  #account=accounts[0]
+  #print(account)
+  #account=account.load("freecodecamp-account")
+  #print(account)
+  
+  def main():
+    deploy_simple_storage()
