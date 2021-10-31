3aecc84c31905782a0b956dc35281bb12e3b542c
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-10-31 19:14:22 +0530

    Update deploy.py
    updated

--- deploy.py
+++ deploy.py
@@ -1,15 +1,22 @@
 #create new file under Brownie-Simple-Storage/scripts
-from brownie import accounts
+from brownie import accounts,config,SimpleStorage
 
 
 
 def deploy_simple_storage():
-  #account=accounts[0]
-  #print(account)
-  #account=account.load("freecodecamp-account")
-  #print(account)
-  account=accounts.add(os.getenv("PRIVATE_KEY"))
-  print(account)
+  account=accounts[0]
+  simple_storage=SimpleStorage.deploy({"from":account})
+  stored_value=simple_storage.retrieve()
+  print(stored_value)
+  transaction=simple_storage.store(15,{"from":account})
+  transaction.wait(1)
+  updated_stored_value=simple_storage.retrieve()
+  print(updated_stored_value)
+  
+  
+
+  
+
                        
   
   def main():
