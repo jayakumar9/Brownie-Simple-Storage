1823e0eefe55063fe223a381915d85757a283524
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-01 05:33:26 +0530

    Update deploy.py
    updated

--- deploy.py
+++ deploy.py
@@ -1,10 +1,10 @@
 #create new file under Brownie-Simple-Storage/scripts
-from brownie import accounts,config,SimpleStorage
+from brownie import accounts,config,SimpleStorage, network
 
 
 
 def deploy_simple_storage():
-  account=accounts[0]
+  account=get_account()
   simple_storage=SimpleStorage.deploy({"from":account})
   stored_value=simple_storage.retrieve()
   print(stored_value)
@@ -12,6 +12,16 @@ def deploy_simple_storage():
   transaction.wait(1)
   updated_stored_value=simple_storage.retrieve()
   print(updated_stored_value)
+  
+def get_account():
+  if network.show_active()=="development":
+    return accounts[0]
+  else:
+    return accounts.add(config["wallets"]["from_key"])
+#see refer vedio 2.solidity at 0:31:00 time   
+  
+  
+  
   
   
 
